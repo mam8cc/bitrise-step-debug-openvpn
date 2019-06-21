9ea7634ca6fe9f478a804d5b2d015e4a4a761871
Mike McClannahan <mike.mcclannahan@equipmentshare.com>
2019-06-21 11:18:42 -0500

    alsbgldg

---
+++ step.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -eu
+
+sudo mkdir ~/logs/openvpn.log
+
+case "$OSTYPE" in
+  linux*)
+    echo "Configuring for Ubuntu"
+
+    echo ${ca_crt} | base64 -d > /etc/openvpn/ca.crt
+    echo ${client_crt} | base64 -d > /etc/openvpn/client.crt
+    echo ${client_key} | base64 -d > /etc/openvpn/client.key
+
+    cat <<EOF > /etc/openvpn/client.conf
+client
+dev tun
+proto ${proto}
+remote ${host} ${port}
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+comp-lzo
+verb 3
+ca ca.crt
+cert client.crt
+key client.key
+EOF
+
+    service openvpn start client > /dev/null 2>&1
+    sleep 5
+
+    if ifconfig | grep tun0 > /dev/null
+    then
+      echo "VPN connection succeeded"
+    else
+      echo "VPN connection failed!"
+      exit 1
+    fi
+    ;;
+  darwin*)
+    echo "Configuring for Mac OS"
+
+    echo ${ca_crt} | base64 -D -o ca.crt > /dev/null 2>&1
+    echo ${client_crt} | base64 -D -o client.crt > /dev/null 2>&1
+    echo ${client_key} | base64 -D -o client.key > /dev/null 2>&1
+
+    sudo openvpn --client --dev tun --proto ${proto} --remote ${host} ${port} --resolv-retry infinite --nobind --persist-key --persist-tun --comp-lzo --verb 3 --ca ca.crt --cert client.crt --key client.key > ~/logs/openvpn.log &
+    ifconfig -l
+
+    sleep 5
+
+    ping 10.0.5.200
+
+    if ifconfig -l | grep utun0 > /dev/null
+    then
+      echo "VPN connection succeeded"
+    else
+      echo "VPN connection failed!"
+      exit 1
+    fi
+    ;;
+  *)
+    echo "Unknown operative system: $OSTYPE, exiting"
+    exit 1
+    ;;
+esac
